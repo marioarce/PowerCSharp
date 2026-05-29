6024edf1469f9ae9f801e5c9ec83d06430c0509d
Mario Alberto Arce <marioalberto.arce@gmail.com>
2026-05-29 17:55:55 -0600

    feat(linq): add IDynamicOrderProvider interface for dynamic ordering support
    - Implement service provider interface for dynamically ordering entities using Dynamic Expressions
    - Support multiple ordering delegates with ascending/descending flags
    - Enable flexible runtime order configuration for LINQ queries
    - Provide foundation for IEnumerableExtensions dynamic ordering capabilities
    - Use List of tuples containing Func delegates and boolean descending flags

---
+++ src/PowerCSharp.Extensions/Linq/Interfaces/IDynamicOrderProvider.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace PowerCSharp.Extensions.Linq.Interfaces;
+
+/// <summary>
+/// Implement a Service Provider for dynamically ordering of the type <typeparamref name="T"/> using Dynamic Expressions.
+/// </summary>
+/// <typeparam name="T">The type of the entity to apply the dynamic order.</typeparam>
+public interface IDynamicOrderProvider<T>
+{
+    /// <summary>
+    /// Sets delegate to use for ordering
+    /// </summary>
+    /// <param name="delegates">A list of predicates that takes an object of type <typeparamref name="T"/> and returns a <see cref="object"/>.</param>
+    void SetOrderDelegates(List<(Func<T, object>, bool)> delegates);
+
+    /// <summary>
+    /// Gets the list of delegates to be used to order the <typeparamref name="T"/>
+    /// </summary>
+    /// <returns>A list of predicates that takes an object of type <typeparamref name="T"/> and returns a <see cref="object"/>.</returns>
+    List<(Func<T, object>, bool)> GetOrderDelegates();
+}
